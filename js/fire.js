1b176ef15422104924cd4fee89381d817535ebd0
stupiddingo <brent@stupiddingo.com>
2016-07-26 15:56:15 -0600

    Remove OSM, change closure to AGS from KMZ.

--- js/fire.js
+++ js/fire.js
@@ -1,4 +1,7 @@
-﻿var map;
+﻿/**
+ * See https://github.com/googlemaps/v3-utility-library
+ */
+var map;
 var kmlLayer;
 var defaultout = ['*']; //default arcgisserver rest outfields
 var maparray = [];
@@ -12,6 +15,20 @@ var minimumlongitude = 180; //lng min
 var maximumlongitude = -180; //lng max
 var minimumlatitude = 90; //lat min
 var maximumlatitude = -90; //lat max
+var res,ovs, iw = [];
+var prev_iw = false;
+var ovOptions = {
+  polylineOptions: {
+    strokeColor: '#FF0000',
+    strokeWeight: 4
+  },
+  polygonOptions: {
+    fillColor: '#FFFF99',
+    fillOpacity: 0.5,
+    strokeWeight: 2,
+    strokeColor: '#FF0000'
+  }
+};
 var hStyle = {
     fillColor: '#ac30b5',
     fillOpacity: 0.05,
@@ -74,10 +91,6 @@ google.setOnLoadCallback(function () {
         dynamap.setMap(map);
         google.maps.event.addListener(dynamap, 'mouseover', function () {
             highlight(dynamap, "", latlng);
-        });
-        google.maps.event.addListener(dynamap, 'click', function () {
-            highlight(dynamap, "", latlng);
-            $("#mapcenterpopulate").html(html);
         });
         // the following use cursor to indicate busy
         google.maps.event.addListener(gmaps.ags.Util, 'jsonpstart', function () {
@@ -198,27 +211,104 @@ google.setOnLoadCallback(function () {
           opacity: opacity
           });
         map.overlayMapTypes.insertAt(1, perimeterLayerType);
-        //var perimeterLayer = new gmaps.ags.MapOverlay(perimeterURL);
-        //perimeterLayer.setMap(map);
+        var closureURL = 'https://fishandgame.idaho.gov/gis/rest/services/External/InciWeb_FireClosures/MapServer';
+        svc = new gmaps.ags.MapService(closureURL);
+  var closure = new gmaps.ags.MapOverlay(svc, {
+    'opacity': 0.8
+  });
+  closure.setMap(map);
+        google.maps.event.addListener(map, 'click', identify);
+        google.maps.event.addListener(map, 'mouseover', function () {
+           map.setOptions({ draggableCursor: 'crosshair' });
+        });
+
         var sm = $($(":jqmData(slidemenu)").data('slidemenu'));
         if (viewport().width > 900) {
             slidemenu(sm);
         }
+        
+        function identify(evt) {
+  clearOverlays();
+  if (res) 
+    res.length = 0;
+    svc.identify({
+    'geometry': evt.latLng,
+    'tolerance': 3,
+    'layerIds': [0],
+    'layerOption': 'all',
+    'bounds': map.getBounds(),
+    'width': map.getDiv().offsetWidth,
+    'height': map.getDiv().offsetHeight,
+    'overlayOptions': ovOptions
+  }, function(results, err) {
+    if (err) {
+      console.log(err.message + err.details.join('\n'));
+    } else {
+      addResultToMap(results, evt.latLng);
+    }
+  });
+}
+
+function clearOverlays() {
+  if (ovs) {
+    for (var i = 0; i < ovs.length; i++) {
+      ovs[i].setMap(null);
+    }
+    ovs.length = 0;
+  }
+}
+
+function addResultToMap(idresults, latlng) {
+          res = idresults.results;
+          layers = { "0": []};
+          for (var i = 0; i < res.length; i++) {
+            var result = res[i];
+            layers[result['layerId']].push(result);
+          }
+          // the following code based on ESRI sample
+          // create and show the info-window with tabs, one for each map service layer
+          for (var layerId in layers) {
+            var results = layers[layerId];
+            var count = results.length;
+            var content = "";
+            switch(layerId) {
+              case "0":
+                if (count == 0) break;
+                for (var j = 0; j < count; j++) {
+                  var attributes = results[j].feature.attributes;
+                  var url = attributes["URL"];
+                  var start = url.indexOf("://");
+                  url = url.substring(start+3);
+                  if (url.length > 40) {
+                    url = url.substring(0,40) + "...";
+                  }
+                  content += "<div><strong>" + attributes["NAME"]  + "</strong><br/>";
+                  content += "Updated: " + attributes["UPDATE_"] + "<br/>Link: <a href='" + attributes["URL"]  + "' title='More Information'>" + url + "</a></div>";
+                }
+                break;
+            }
+            if (content != "") {
+              var iw = new google.maps.InfoWindow({
+                content: content,
+                position: latlng
+              });
+              if (prev_iw) {
+                prev_iw.close();
+              }
+              prev_iw = iw;
+              iw.open(map);
+              
+            }
+          }
+        }
+
+
+
+
+
+
     });
     var josefov = new google.maps.LatLng(45.3119, -116.67029);
-    //Define OSM as base layer in addition to the default Google layers
-    var osmMapType = new google.maps.ImageMapType({
-        getTileUrl: function (coord, zoom) {
-            return "http://otile1.mqcdn.com/tiles/1.0.0/osm/" +
-                zoom + "/" + coord.x + "/" + coord.y + ".png";
-        },
-        tileSize: new google.maps.Size(256, 256),
-        isPng: true,
-        alt: "OpenStreetMap",
-        name: "Streets",
-        maxZoom: 19,
-        opacity: 0.7
-    });
     //Define custom WMS tiled layer
     var SLPLayer = new google.maps.ImageMapType({
         getTileUrl: function (coord, zoom) {
@@ -250,6 +340,7 @@ google.setOnLoadCallback(function () {
             url += "&WIDTH=256";         //tile size in google
             url += "&HEIGHT=256";
             return url;                 // return URL for the tile
+						console.log(url);
         },
         tileSize: new google.maps.Size(256, 256),
         isPng: true
@@ -258,31 +349,17 @@ google.setOnLoadCallback(function () {
         var mapOptions = {
             zoom: 7,
             center: josefov,
-            mapTypeId: 'OSM',
+            scaleControl: true,
             mapTypeControlOptions: {
-                mapTypeIds: ['OSM', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.HYBRID, google.maps.MapTypeId.TERRAIN],
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.HYBRID, google.maps.MapTypeId.TERRAIN],
                 style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
             }
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-        map.mapTypes.set('OSM', osmMapType);
         map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
         //add WMS layer
         SLPLayer.setOpacity(0.4);
         map.overlayMapTypes.push(SLPLayer);
-        var closureLayer = new google.maps.KmlLayer('https://fishandgame.idaho.gov/gis/rest/services/External/InciWeb_FireClosures/MapServer/0/query?where=1+%3D+1&outFields=*&returnGeometry=true&outSR=4326&returnIdsOnly=false&returnCountOnly=false&f=kmz');
-        closureLayer.setMap(map);
-    }
-    function kml(surl) {
-        if (surl != "") {
-            //clearOverlays();
-            kmlLayer = new google.maps.KmlLayer(surl, {
-                preserveViewport: true,
-                clickable: true,
-                suppressInfoWindows: false
-            });
-            map.currentLayer = kmlLayer;
-        }
     }
     initialize();
     $("#clearmap").click(
